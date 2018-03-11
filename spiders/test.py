06070e57262e88e61bebe160242cf6b7a9ce3706
lichen <lichen@kangfuzi.cn>
2018-03-11 17:47:00 +0800

    test

--- spiders/test.py
+++ spiders/test.py
@@ -1,5 +1,34 @@
-# coding=utf-8
-from handle import tools
+# -*- coding:utf-8 -*-
+from bs4 import BeautifulSoup
+from handler import tools
+import re
 
-data = tools.ungzip_file('5c127b79d51710ed26a3f2a9389b1c46.html')
-print data
+source = tools.ungzip_file('link_1e37b1faa6057996395484fc25d8ee0c.html')
+print source
+if source is not None:
+    try:
+        s = source.strip()
+        s = s.replace('\t','').replace('\r','').replace(' ','')
+        re_cdata=re.compile('//<!\[CDATA\[[^>]*//\]\]>',re.I) #匹配CDATA
+        re_script=re.compile('<\s*script[^>]*>[^<]*<\s*/\s*script\s*>',re.I) #Script
+        re_style=re.compile('<\s*style[^>]*>[^<]*<\s*/\s*style\s*>',re.I) #style
+        re_br=re.compile('<br\s*?/?>') #处理换行
+        re_comment=re.compile('<!--[^>]*-->') #HTML注释
+        blank_line = re.compile('\n+') #处理空行
+        s=re_cdata.sub('',s) #去掉CDATA
+        s=re_script.sub('',s) #去掉SCRIPT
+        s=re_style.sub('',s) #去掉style
+        s=re_br.sub('\n',s) #将br转换为换行
+        s=re_comment.sub('',s) #去掉HTML注释
+        s=blank_line.sub('\n',s) #去掉多余的空行
+        # print s
+        p1 = re.compile("src=\"(.+?)\"class=\"attachment-list-thumbwp-post-image\"")
+        url1 = p1.search(s)
+        print 'url1 is : ' + url1.group(1)
+        p2 = re.compile("loadsrc=\"(.+?)\"")
+        urls = p2.findall(s)
+        for url in urls:
+            print url
+
+    except Exception,e:
+        print e
